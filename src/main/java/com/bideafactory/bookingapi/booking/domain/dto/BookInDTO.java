bff1f8250936d5b9edbbd4b41634b1c758099916
Leonardo Molina <dev@xempre.com>
2024-07-22 01:30:12 -0500

    Add validation to DTO

--- src/main/java/com/bideafactory/bookingapi/booking/domain/dto/BookInDTO.java
+++ src/main/java/com/bideafactory/bookingapi/booking/domain/dto/BookInDTO.java
@@ -1,21 +1,34 @@
 package com.bideafactory.bookingapi.booking.domain.dto;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 import lombok.Data;
 
+import jakarta.validation.constraints.*;
+
 @Data
 public class BookInDTO {
+    @NotBlank
+    @NotNull
     private String id;
 
+    @NotBlank
+    @NotNull
     private String name;
+    @NotBlank
+    @NotNull
     private String lastname;
+    @NotNull
     private Integer age;
+    @NotBlank
     private String phoneNumber;
 
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
+    @NotNull
+    private LocalDate startDate;
+    @NotNull
+    private LocalDate endDate;
 
+    @NotBlank
     private String houseId;
     private String discountCode;
 }
